95a33a96c12e2404fc470ace9b7f61a84816b7ec
Fotis Kapotos <fotiskapotos@gmail.com>
2022-01-22 18:41:38 +0100

    Add files via upload

--- helper.py
+++ helper.py
@@ -7,6 +7,8 @@ Created on Fri Oct  8 19:49:43 2021
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal as sg
 
 
 
@@ -31,16 +33,48 @@ def get_double_column_data(filepath):
     filtered = data[2:len(data):2]
     
     raw_list = '\n'.join(raw)
-    filtered_list = '\n'.join(filtered)
     
+    filtered_list = '\n'.join(filtered)
+    #print(filtered_list)
     raw_signal = [int(s) for s in raw_list.split() if s.isdigit()]
     filtered_signal = [int(s) for s in filtered_list.split() if s.isdigit()]
     
-    return raw_signal, filtered_signal
+    return np.array(raw_signal), np.array(filtered_signal)
  
    
    
 
 def create_time_axes(data):
-    return np.linspace(0,np.size(data)*2,np.size(data))
+    return np.linspace(0,np.size(data)*4,np.size(data))
+
+
+def filter(signal, lpfreq,order, highpass = False, plot=False):
+    rms = np.sqrt(np.mean(signal**2))
+    if highpass == True:
+        hp = sg.butter(1, 5, 'hp', fs=1000, output='sos')
+        signal = sg.sosfilt(hp, signal)
+        
+    rectified_signal = np.abs(signal-rms)
+    lp = sg.butter(order, lpfreq, 'lp', fs=1000, output='sos',)
+    output = sg.sosfilt(lp,rectified_signal)
+    
+    if plot==True:
+        time = create_time_axes(signal)
+        fig, ax = plt.subplots()
+        ax.plot(time, output,'m')
+        ax.set_title("Output signal")
+        ax.set_xlabel("time")
+    
+    return output
+
+
+
+
+
+
+
+
+
+
+
 
